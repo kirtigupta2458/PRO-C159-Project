9585bb50604bf130c0a2098b7f818118c21a3915
kirtigupta2458 <72331388+kirtigupta2458@users.noreply.github.com>
2023-05-10 06:30:32 +0530

    Update infoBanner.js

--- js/infoBanner.js
+++ js/infoBanner.js
@@ -67,45 +67,17 @@ AFRAME.registerComponent("info-banner", {
   },
   createImageEl: function (item) {
     const entityEl = document.createElement("a-entity");
-    entityEl.setAttribute("visible", true);
-    entityEl.setAttribute("geometry", {
-      primitive: "plane",
-      width: 0.85,
-      height: 0.4,
-    });
-    entityEl.setAttribute("material", { src: item.banner_url });
-    entityEl.setAttribute("position", { x: 0, y: 0.3, z: 0.05 });
+    
     return entityEl;
   },
   createTitleEl: function (item) {
     const entityEl = document.createElement("a-entity");
-    entityEl.setAttribute("visible", true);
-    entityEl.setAttribute("text", {
-      shader: "msdf",
-      anchor: "left",
-      font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
-      width: 1.2,
-      height: 2,
-      color: "#fff",
-      value: `${item.title} (${item.released_year})`,
-    });
-    entityEl.setAttribute("position", { x: -0.4, y: 0.02, z: 0.05 });
+    
     return entityEl;
   },
   createDescriptionEl: function (item) {
     const entityEl = document.createElement("a-entity");
-    entityEl.setAttribute("visible", true);
-    entityEl.setAttribute("text", {
-      shader: "msdf",
-      anchor: "left",
-      font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
-      width: 0.75,
-      height: 2,
-      color: "#fff",
-      wrapCount: "40",
-      value: item.description,
-    });
-    entityEl.setAttribute("position", { x: -0.4, y: -0.24, z: 0.05 });
+    
     return entityEl;
   },
 });
